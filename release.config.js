5653ec9ae6fb244e98b4110564f90794a5a7e123
jburtnett <jburtnett@clearwateranalytics.com>
2022-04-14 14:06:49 -0600

    format: Adjusted formatting

--- release.config.js
+++ release.config.js
@@ -5,12 +5,13 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
-        "@semantic-release/github", {
-            assets: [
-                { path: "build.zip", label: "Build" },
-                { path: "coverage.zip", lable: "Coverage"}
-            ]
-        }
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", lable: "Coverage" }
+        ]
+      }
     ]
   ]
 };
